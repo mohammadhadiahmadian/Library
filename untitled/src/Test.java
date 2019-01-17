262ebe5de0d8d0112a6da16e6be422474a5716d8
MohammadHadi <mohammadhadiahmadian1999@gmail.com>
2019-01-17 12:34:30 +0330

    First commit just for test

--- untitled/src/Test.java
+++ untitled/src/Test.java
@@ -2,9 +2,8 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
-        Date d = new Date();
-        d.setHours(336 + d.getHours());
-        d.setDate();
-        System.out.println(d);
+        //Date d = new Date();
+        //d.setHours(336 + d.getHours());
+        //System.out.println(d);
     }
 }
