4ea5054227f8b5044d467f39758f04904e6c8b20
MohammadHadi <mohammadhadiahmadian1999@gmail.com>
2019-01-20 19:31:07 +0330

    Thesis class

---
+++ gittest/src/Thesis.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+public class Thesis extends Source {
+
+    private String author;
+
+    public enum authorGrade {
+        BSC,
+        MSC,
+        PHD
+    }
+
+    authorGrade grade;
+
+    public Thesis(String title, Date publishDate, String author, authorGrade grade) {
+        super(title, publishDate);
+        setAuthor(author);
+        setGrade(grade);
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setGrade(authorGrade grade) {
+        this.grade = grade;
+    }
+
+    public authorGrade getGrade() {
+        return this.grade;
+    }
+
+}
