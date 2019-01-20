440a68f5f5cbeb6453dd845d89db2b0e47abe06b
MohammadHadi <mohammadhadiahmadian1999@gmail.com>
2019-01-20 15:40:05 +0330

    v1

--- untitled/src/Test.java
+++ untitled/src/Test.java
@@ -2,8 +2,7 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
-        //Date d = new Date();
-        //d.setHours(336 + d.getHours());
-        //System.out.println(d);
+        System.out.println("hello");
+        System.out.println("bye");
     }
 }
