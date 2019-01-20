b653f3e7c2a04db382289b12b684af25e5f948af
MohammadHadi <mohammadhadiahmadian1999@gmail.com>
2019-01-20 17:43:10 +0330

    random

--- gittest/src/Main.java
+++ gittest/src/Main.java
@@ -1,5 +1,10 @@
+import javax.sound.midi.Soundbank;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Random random = new Random();
+        System.out.println(random.nextInt());
     }
 }
