4edd561a849dc2dc8b29c134a0d20ac590ceb294
MohammadHadi <mohammadhadiahmadian1999@gmail.com>
2019-01-21 17:25:36 +0330

    Search in sources added

--- gittest/src/Main.java
+++ gittest/src/Main.java
@@ -1,7 +1,37 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    private ArrayList<Member> members = new ArrayList<>();
+    private ArrayList<Source> sources = new ArrayList<>();
 
     public static void main(String[] args) {
 
+    }
+
+    public ArrayList<Member> getMembers() {
+        return this.members;
+    }
+
+    public void addMember(Member member) {
+        this.members.add(member);
+    }
+
+    public ArrayList<Source> getSources() {
+        return this.sources;
+    }
+
+    public void addSource(Source source) {
+        this.sources.add(source);
+    }
+
+    public ArrayList<Source> searchSource(String search) {
+        ArrayList<Source> result = new ArrayList<>();
+        for (Source source : this.getSources()) {
+            if (source.getTitle().contains(search))
+                result.add(source);
+        }
+        return result;
     }
 
 }
