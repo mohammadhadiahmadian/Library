8830b48b9d56fb2a659ae7c998c13e5b4eae8284
MohammadHadi <mohammadhadiahmadian1999@gmail.com>
2019-01-21 17:15:01 +0330

    Main class changed

--- gittest/src/Main.java
+++ gittest/src/Main.java
@@ -1,54 +1,7 @@
-import java.util.ArrayList;
-import java.util.Date;
-
 public class Main {
+
     public static void main(String[] args) {
 
-        ArrayList<Source> sources;
-        ArrayList<Member> members;
-
-    }
-
-    public void borrowSource(Source source, Member member) {
-        source.borrowing(member);
-        member.borrowSource(source);
-    }
-
-    public void returnSource (Source source, Member member) {
-        source.setReturnDate();
-    }
-
-    public ArrayList<Member> Infringements(ArrayList<Member> members) {
-        ArrayList<Member> result = new ArrayList<>();
-        for (Member member : members) {
-            for (Source source : member.getBorrows()) {
-                if (source.getReturnDateLimit().before(new Date())) {
-                    result.add(member);
-                    break;
-                }
-            }
-        }
-        return result;
-    }
-
-    public ArrayList<Member> searchMembers(ArrayList<Member> members, String searchPhrase) {
-        ArrayList<Member> result = new ArrayList<>();
-        for (Member member : members) {
-            if (member.getName().contains(searchPhrase) || member.getFamilyName().contains(searchPhrase)) {
-                result.add(member);
-            }
-        }
-        return result;
-    }
-
-    public ArrayList<Source> searchSources(ArrayList<Source> sources, String searchPhrase) {
-        ArrayList<Source> result = new ArrayList<>();
-        for (Source source : sources) {
-            if (source.getTitle().contains(searchPhrase)) {
-                result.add(source);
-            }
-        }
-        return result;
     }
 
 }
