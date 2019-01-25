45486e5f702e17061b7f4720cba00fd15af63e30
MohammadHadi <mohammadhadiahmadian1999@gmail.com>
2019-01-26 01:56:58 +0330

    Getting ready /:

--- gittest/src/Main.java
+++ gittest/src/Main.java
@@ -1,7 +1,41 @@
+import java.io.File;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public void addSource(File file) {
+
+    }
+
+    public void addMember(File file) {
+
+    }
+
+    public void borrowSource(String Sourcecode, String Membercode) {
+
+    }
+
+    public void returnSource(String Sourcecode, String Membercode) {
+
+    }
+
+    public ArrayList<Source> searchSource(String search) {
+        ArrayList<Source> result = new ArrayList<>();
+        return result;
+    }
+
+    public ArrayList<Member> searchMember(String search) {
+        ArrayList<Member> result = new ArrayList<>();
+        return result;
+    }
+
+    public ArrayList<Member> timeoutInfractions () {
+        ArrayList<Member> result = new ArrayList<>();
+        return result;
     }
 
 }
